af562f52ff0db1fbbb4085673de785f0d960aea4
Bernd Warmuth <bernd.warmuth@gmail.com>
2012-09-19 22:16:16 +0200

    added syntax.xsd, check script against xsd

--- src/at/ac/tuwien/complang/programmiersprachen/FileUtil.groovy
+++ src/at/ac/tuwien/complang/programmiersprachen/FileUtil.groovy
@@ -1,5 +1,9 @@
 package at.ac.tuwien.complang.programmiersprachen
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+
 /**
  * 
  * @author bernd warmuth <bernd.warmuth@gmail.com>
@@ -7,11 +11,14 @@ package at.ac.tuwien.complang.programmiersprachen
  */
 class FileUtil {
 
-
 	static void main(args) {
-		println 'hello'
-		
-		//parsing testfile
+		println 'parsing script file'
 		def records = new XmlSlurper().parse("./examplefile.xml")
+
+		def factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+		def schema = factory.newSchema(new File("syntax.xsd"))
+		def validator = schema.newValidator()
+		println 'validating script file'
+		validator.validate(new StreamSource(new File("examplefile.xml")))
 	}
 }
